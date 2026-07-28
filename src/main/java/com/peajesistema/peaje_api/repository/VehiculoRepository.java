6e77d84f1a1097d70bb57ac9b993454cdf1a1716
Joel Montero <monterojoel135@gmail.com>
2026-07-28 09:15:03 -0400

    Actualizar VehiculoRepository.java
    comments eliminate

--- src/main/java/com/peajesistema/peaje_api/repository/VehiculoRepository.java
+++ src/main/java/com/peajesistema/peaje_api/repository/VehiculoRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository // Le dice a Spring: "Este componente maneja la base de datos"
 public interface VehiculoRepository extends JpaRepository<Vehiculo, String> {
-    // ¡NO ESCRIBES CÓDIGO! 
-    // Al heredar de JpaRepository<Clase, TipoDeLlavePrimaria>,
     // Spring Boot te regala métodos como: .findById(), .save(), .delete(), .findAll()
 }
